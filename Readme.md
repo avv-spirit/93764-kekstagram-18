37b271250305da1843a283a702c9beed36461c4b
Vladimir <Sp1rit911@mail.ru>
2019-10-03 11:40:23 +0300

    Делаю массив и циклы

--- Readme.md
+++ Readme.md
@@ -1,7 +1,7 @@
 # Личный проект «Кекстаграм» [![Build status][travis-image]][travis-url]
 
 * Студент: [Владимир Азаров](https://up.htmlacademy.ru/javascript/18/user/93764).
-* Наставник: `Неизвестно`.
+* Наставник: [Байдаров Денис] (https://up.htmlacademy.ru/adaptive/15/user/7956).
 
 ---
 
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 'use strict';
-var arrNames = ['Joel', 'Ellie', 'Tess', 'Bill', 'Tommy', 'Sarah', 'Marlene', 'Henry', 'Sam', 'Maria', 'James', 'Robert', 'Niel', 'Ashley', 'Tom', 'Nathan', 'Bruce', 'Troy', 'Max', 'Vladimir', 'Alex', 'Zhanna', 'Serge', 'Leo', 'Keanu'];
-var arrComments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают.', 'Как можно было поймать такой неудачный момент?!'];
+var ARR_NAMES = ['Joel', 'Ellie', 'Tess', 'Bill', 'Tommy', 'Sarah', 'Marlene', 'Henry', 'Sam', 'Maria', 'James', 'Robert', 'Niel', 'Ashley', 'Tom', 'Nathan', 'Bruce', 'Troy', 'Max', 'Vladimir', 'Alex', 'Zhanna', 'Serge', 'Leo', 'Keanu'];
+var ARR_COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают.', 'Как можно было поймать такой неудачный момент?!'];
+var ARR_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ,12 ,13];
 
 var toGetNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -24,9 +25,19 @@ var calcAvatars = function (amountAvatars) {
   return amountAvatars;
 };
 
+var calcNumbers = function (amountNumbers) {
+  var arrNumbers = [];
+  for (var i = 1; i <= amountNumbers; i++) {
+    var numbers = i ;
+    arrNumbers.push(numbers);
+  }
+  return amountNumbers;
+};
+
+
 var userObject = {
   'avatars': 'img/avatar-' + toGetNumber(1, 6) + '.svg',
-  'comments': arrComments[toGetNumber(0, arrComments.length)],
-  'name': arrNames[toGetNumber(0, arrNames.length)]
+  'comments': ARR_COMMENTS[toGetNumber(0, ARR_COMMENTS.length)],
+  'name': ARR_NAMES[toGetNumber(0, ARR_NAMES.length)]
 };
 

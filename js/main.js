23090df36c2cd98b71eb605284c5cb40b32d6c1c
avv-spirit <Sp1rit911@mail.ru>
2019-10-05 02:00:01 +0300

    Функции

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 'use strict';
 var ARR_NAMES = ['Joel', 'Ellie', 'Tess', 'Bill', 'Tommy', 'Sarah', 'Marlene', 'Henry', 'Sam', 'Maria', 'James', 'Robert', 'Niel', 'Ashley', 'Tom', 'Nathan', 'Bruce', 'Troy', 'Max', 'Vladimir', 'Alex', 'Zhanna', 'Serge', 'Leo', 'Keanu'];
 var ARR_COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают.', 'Как можно было поймать такой неудачный момент?!'];
-var ARR_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ,12 ,13];
+
 
 var toGetNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
-
+/*
 var calcUrl = function (amountNumbers) {
   var arrPhotoNumbers = [];
   for (var i = 1; i <= amountNumbers; i++) {
@@ -24,16 +24,17 @@ var calcAvatars = function (amountAvatars) {
   }
   return amountAvatars;
 };
+*/
 
-var calcNumbers = function (amountNumbers) {
+// Функцию ниже сделал для массива с числами
+
+var calcNumbers = function () {
   var arrNumbers = [];
-  for (var i = 1; i <= amountNumbers; i++) {
-    var numbers = i ;
-    arrNumbers.push(numbers);
+  for (var i = 1; i <= 25; i++) {
+    arrNumbers.push(i);
   }
-  return amountNumbers;
+  return arrNumbers;
 };
-
 
 var userObject = {
   'avatars': 'img/avatar-' + toGetNumber(1, 6) + '.svg',
@@ -41,3 +42,5 @@ var userObject = {
   'name': ARR_NAMES[toGetNumber(0, ARR_NAMES.length)]
 };
 
+// Денис, я дальше запустался, как дальше сделать массив объектов userObject на 25 раз и магические числа заменить с 1 до 6 через массив, который не будет вываливаться весь.
+// Понимаю, что надо сделать функцию, в ней цикл по перебору длинны массива (arrNumbers.length в котором 25 чисел), но вот как назначить эти числа объекту хз. Нужна твоя помощь.
